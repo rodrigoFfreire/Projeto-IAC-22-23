b87cdbb41120207db37f9d8482cd3db76d4bc8b1
davidrr21 <david8rr@gmail.com>
2023-06-02 16:17:02 +0100

    Rotina de conversão Hexadecimal para Decimal-correção

--- src/hexdec.asm
+++ src/hexdec.asm
@@ -3,7 +3,7 @@
 rot_hexa_dec:
     PUSH R1
     PUSH R2
-    PUSH R10
+    PUSH R9
 
     MOV R1, 1000
     MOV R2, 10
@@ -11,13 +11,13 @@ rot_hexa_dec:
     MOV R8, 0 ;inicialização do resultado decimal
 
     loop_hexa_dec:
-        MOD R10, R1 
+        MOD R9, R1 
         DIV R1, R2
 
         CMP R1, 0 ; se for 0 termina o loop
         JNZ ret_hexa_dec
 
-        MOV R3, R10
+        MOV R3, R9
         DIV R3, R1
 
         SHL R8, 4 ;desloca para a esquerda para a entrada do novo digito
@@ -26,7 +26,7 @@ rot_hexa_dec:
         JMP loop_hexa_dec
 
     ret_hexa_dec:
-    POP R10
+    POP R9
     POP R3
     POP R2
     POP R1
